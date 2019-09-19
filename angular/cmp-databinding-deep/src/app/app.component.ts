52d4fb30f1f4b8d9e4a4ecd5181df63e5a8df1fd
imdadz <imdadz>
2019-09-19 22:56:28 +0530

    LifeCycle hooks and Template access

--- angular/cmp-databinding-deep/src/app/app.component.ts
+++ angular/cmp-databinding-deep/src/app/app.component.ts
@@ -26,4 +26,12 @@ export class AppComponent {
     });
   }
 
+  onChangeFirst(){
+    this.serverElements[0].name = "changed";
+  }
+
+  OnDestroyFirst(){
+    this.serverElements.splice(0,1);
+  }
+
 }
