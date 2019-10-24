c8d65193baefb73c3f9bfd64464741ec8cd1273f
imdadz <imdadone@gmail.com>
2019-10-24 16:27:25 +0530

    Add Ingredients to Shopping list

--- angular/project-cmp-databinding/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ angular/project-cmp-databinding/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild, EventEmitter, Output } from '@angular/core';
+import { Ingredient } from 'src/app/shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -7,9 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShoppingEditComponent implements OnInit {
 
+  @ViewChild('nameInput',{static:false}) nameInputRef:ElementRef;
+  @ViewChild('amountInput',{static:false}) amountInputRef:ElementRef;
+  @Output() ingredientadded = new EventEmitter<Ingredient>();
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  OnAdded(){
+    const ingName = this.nameInputRef.nativeElement.value;
+    const ingAmount = this.amountInputRef.nativeElement.value;
+    const newIngredient = new Ingredient (ingName,ingAmount);
+    this.ingredientadded.emit(newIngredient);
   }
 
 }
